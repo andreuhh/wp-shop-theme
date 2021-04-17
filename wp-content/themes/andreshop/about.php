ad9193c44e248dacf28ff68d89fbfc0e730adfd7
Andrea Guffi <andreaguffi92@gmail.com>
2021-04-17 14:15:37 +0200

    ok

---
+++ wp-content/themes/andreshop/about.php
@@ -0,0 +1,61 @@
+<!--
+   Template Name: About
+-->
+<?php get_header(); ?>
+<div class="content-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+            <div class="hero"></div>
+
+            <h3 class="pt-5">About us</h3>
+            <p class="pb-5">E se non puoi passare a trovarci perché abiti troppo distante, visita il nostro e-commerce. Qui trovi a portata di click ogni singolo articolo disponibile in negozio; basta un click ed un paio di giorni lavorativi riceverai il pacco a casa.</p>
+
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                    </div>
+                    <div class="form-group col-md-6">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                    </div>
+                </div>
+            
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                    </div>
+                    <div class="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                    </div>
+                    <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Check me out
+                    </label>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
